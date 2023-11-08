0cef54863c6f8a89b4f4a1b857995e386530e677
Florian Ceprika <f.ceprika@gmail.com>
2023-11-08 19:04:45 +0100

    push ga

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ module.exports = {
     'Florian Ceprika, Florian, Ceprika, software engineer, front-end engineer, web developer, php developer, magento',
   siteUrl: 'https://florianceprika.com',
   siteLanguage: 'en_US',
-  googleAnalyticsID: '6360506423',
+  googleAnalyticsID: 'G-SC07T3T66C',
   googleVerification: '111',
   name: 'Florian Ceprika',
   location: 'Paris, France',
